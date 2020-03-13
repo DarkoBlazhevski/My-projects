f02041ed433344e0f10c3c864f63c0dd0b91a2f4
DarkoBlazhevski <DarkoBlazhevski@users.noreply.github.com>
2020-03-13 19:47:58 +0100

    new

---
+++ react-redux-complete/myapp/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Ninjas from "./Ninjas";
+import AddNinja from "./AddNinja";
+
+class App extends React.Component {
+  state = {
+    ninjas: [
+      { name: "Ryu", age: 30, belt: "black", id: 1 },
+      { name: "Yoshi", age: 20, belt: "green", id: 2 },
+      { name: "Crystal", age: 25, belt: "pink", id: 3 }
+    ]
+  };
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    });
+  }
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    });
+    this.setState({
+      ninjas: ninjas
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>welcome</h1>
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} />
+        <AddNinja addNinja={this.addNinja} />
+      </div>
+    );
+  }
+}
+export default App;
